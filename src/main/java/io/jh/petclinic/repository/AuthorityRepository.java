5b5a47485f1c4ef28f3a8fe6098b0549ec0cba1a
belyaev-andrey <belyaev.andrey@gmail.com>
2022-06-28 10:16:43 +0300

    Initial version of jhpetclinic generated by generator-jhipster@7.8.1

---
+++ src/main/java/io/jh/petclinic/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package io.jh.petclinic.repository;
+
+import io.jh.petclinic.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/io/jh/petclinic/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.jh.petclinic.service.dto;
